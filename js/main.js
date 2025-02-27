cada50c779a5e27cf2d25e3317b84a7fbb289806
lbascl2 <lbascl2@lsu.edu>
2025-02-27 08:37:26 -0600

    Responsiveness and content
    started responsive nav bar
    
    started form
    
    added content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // JS scripts placed here
+
+// HAMBURGER ICONE
+const hamburger = document.querySelector('.hamburger'); 
+const menu = document.getElementById('main-menu');
+hamburger.addEventListener('click', () => {   const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;   
+    hamburger.setAttribute('aria-expanded',!expanded);   
+    menu.classList.toggle('expanded'); });
