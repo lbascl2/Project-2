a4ce0ce105d9ece0cb0bbc3576a0935798d3d5fb
lbascl2 <lbascl2@lsu.edu>
2025-02-28 07:15:54 -0600

    updates to all sections
    did things to all

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 // JS scripts placed here
 
-// HAMBURGER ICONE
-const hamburger = document.querySelector('.hamburger'); 
-const menu = document.getElementById('main-menu');
-hamburger.addEventListener('click', () => {   const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;   
-    hamburger.setAttribute('aria-expanded',!expanded);   
-    menu.classList.toggle('expanded'); });
+// HAMBURGER ICON
+
+
