d9f99f1989df2923b752cf43b2e6944c07137825
lbascl2 <lbascl2@lsu.edu>
2025-02-28 22:26:18 -0600

    Responsive Nav Menu
    Basis of the responsive nav menu is down

--- js/main.js
+++ js/main.js
@@ -2,4 +2,20 @@
 
 // HAMBURGER ICON
 
+// const hamburger = document.querySelector('.hamburger');
+// const menu = document.getElementById('main-menu');
 
+// hamburger.addEventListener('click', () => {
+//   const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+//   hamburger.setAttribute('aria-expanded',!expanded);
+//   menu.classList.toggle('expanded');
+// });
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
